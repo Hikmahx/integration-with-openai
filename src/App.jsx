300d4a4ab86387d50d81f6f08abe223b4458dc69
Hikmahx <hikmayousuph@gmail.com>
2023-09-18 08:45:52 +0100

    clean up and set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App bg-slate-200 h-screen ">
+      <h1 className="text-purple-950">OpenAI Integration with React</h1>
+    </div>
+  );
+}
+
+export default App;
